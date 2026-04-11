387cb0b1facbc6dd3146c1dda36597502fec4693
m-siede <siede.morgain@gmail.com>
2026-04-11 19:05:40 +0100

    tidied up code, removed time duplicates from days, removed days that were not complete

--- code/main.py
+++ code/main.py
@@ -12,121 +12,158 @@ def main():
     if combined_csv.exists():
         combined_statistics = pd.read_csv(combined_csv)
     else:
-        schizophrenia_statistics, schizophrenia_data = read_group_files('schizophrenia', 22)
-        control_statistics, control_data = read_group_files('control', 32)
+        combined_statistics = create_combined_dataset(combined_csv)
 
-        # Group data and combine into one dataframe
-        schizophrenia_statistics['group'] = 'schizophrenia'
-        control_statistics['group'] = 'control'
-        combined_statistics = pd.concat([schizophrenia_statistics, control_statistics], ignore_index=True)
-        # combined_statistics = combined_statistics.set_index('Unnamed: 0', drop=True)
-        combined_statistics.index.name = 'person_id'
+
+def create_combined_dataset(combined_csv):
+    schizophrenia_statistics, schizophrenia_data = read_group_files('schizophrenia', 22)
+    control_statistics, control_data = read_group_files('control', 32)
+
+    # Group data and combine into one dataframe
+    schizophrenia_statistics['group'] = 'schizophrenia'
+    control_statistics['group'] = 'control'
+    combined_groups_statistics = pd.concat([schizophrenia_statistics, control_statistics], ignore_index=True)
+    # combined_statistics = combined_statistics.set_index('Unnamed: 0', drop=True)
+    combined_groups_statistics.index.name = 'person_id'
         
-        combined_csv.parent.mkdir(exist_ok=True, parents=True)
-        combined_statistics.to_csv(combined_csv)
+    combined_groups_statistics = save_to_csv(combined_groups_statistics, combined_csv)
 
     # Box plots of mean, max and SD of activity for groups
     for col in ['mean', 'max', 'sd']:
         plots(
-            combined_statistics, x='group', y=col, kind='box', show=False, legend=False,
+            combined_groups_statistics, x='group', y=col, kind='box', show=False, legend=False,
             output_path=Path(f'plots/summary/box_ALL_{col}.png'), 
             figsize=(10, 8),
             title=f"Comparison of {col} activity between groups"
         )
-
+    return combined_groups_statistics
 
 def read_group_files(group_name: str, num_files: int) -> pd.DataFrame:
     """Read CSV files for a specified group (patients/control/etc.) and evaluate."""
     statistics_csv = Path(f"data/outputs/{group_name}_statistics.csv")
     all_data_csv = Path(f"data/outputs/{group_name}_data.csv")
 
-    # Read CSV file if it exists
+    # Read CSV file if it exists, else import patient data and statistics and combine into one file
     if statistics_csv.exists() and all_data_csv.exists():
-        all_statistics = pd.read_csv(statistics_csv)
-        all_data = pd.read_csv(all_data_csv)
+        combined_patients_statistics = pd.read_csv(statistics_csv)
+        combined_patients_data = pd.read_csv(all_data_csv)
     else:
-        all_stats = []
-        all_dataframes = []
-        for i in range(1, num_files + 1):
-            print(i)
-            plot= i<5
-            statistics, patient_data = read_patient_file(i, group_name, plot=plot)
+        all_patients_stats = []
+        all_patients_data = []
+        for person_id in range(1, num_files + 1):
+            print(person_id)
+            plot= person_id<5
+            patient_statistics, patient_data = read_patient_file(person_id, group_name, plot=plot)
 
-            for key, value in statistics["activity_by_hour"].items():
-                statistics[f"mean_activity_{key}"] = value
-            del statistics["activity_by_hour"]
+            for key, value in patient_statistics["activity_by_hour"].items():
+                patient_statistics[f"mean_activity_{key}"] = value
+            del patient_statistics["activity_by_hour"]
 
-            patient_data["person_no"] = i
+            patient_data["person_no"] = person_id
 
-            all_stats.append(statistics)
-            all_dataframes.append(patient_data)
+            all_patients_stats.append(patient_statistics)
+            all_patients_data.append(patient_data)
     
         # Save as CSV
-        all_statistics = pd.DataFrame(all_stats, index=range(1, num_files + 1))
-        statistics_csv.parent.mkdir(exist_ok=True, parents=True)
-        all_statistics.to_csv(statistics_csv)
-
-        all_activity = pd.concat(all_dataframes, ignore_index=True)
+        combined_patients_statistics = save_to_csv(all_patients_stats, statistics_csv, index=range(1, num_files + 1))
         
-        all_data = pd.DataFrame(all_activity)
+
+        all_patients_activity_data = pd.concat(all_patients_data, ignore_index=True)
+        
+        combined_patients_data = pd.DataFrame(all_patients_activity_data)
         all_data_csv.parent.mkdir(exist_ok=True, parents=True)
-        all_activity.to_csv(all_data_csv)
+        all_patients_activity_data.to_csv(all_data_csv)
 
         # Create Subdirectory for summary plots
+        # plots_subdir = Path('plots/summary/')
+
+        # # Bar plot of mean activity per hour per person in group
+        # combined_patients_statistics['person_no'] = combined_patients_statistics.index
+        # plots(
+        #     combined_patients_statistics, x='person_no', y='mean', kind='bar', show=False, legend=False,
+        #     title= f"Bar chart of mean activity per person in the {group_name} group",
+        #     output_path=plots_subdir / f"bar_{group_name}_mean_activity.png"
+        # )
+
+        # # Box plot of activity per person in group
+        # plots(
+        #     all_patients_activity_data, x='person_no', y="activity", kind='box', legend=False,
+        #     show=False,title= f"Boxplot of activity per person in the {group_name} group", 
+        #     output_path=plots_subdir / f"box_{group_name}_activity.png"
+        # )
+    
+    return combined_patients_statistics, combined_patients_data
+
+
+def read_patient_file(patient_num: int, group_name: str, plot=True):
+    """Read data for each patient, calculate their individual statistics and create plots"""
+    patient_data = pd.read_csv(f"data/{group_name}/{group_name}_{patient_num}.csv")
+    
+    # create new var for time and date
+    patient_data["timestamp"] = pd.to_datetime(patient_data["timestamp"])
+    patient_data = patient_data.drop_duplicates(subset="timestamp")
+    patient_data["date"] = patient_data["timestamp"].dt.date
+    patient_data["hour"] = patient_data["timestamp"].dt.hour.astype(int)
+    
+    # Remove incomplete days
+    full_days = patient_data.groupby("date").size()
+    full_days = full_days[full_days >= 1400].index
+    patient_data = patient_data[patient_data["date"].isin(full_days)]
+    
+    patient_statistics = extract_features(patient_data)
+
+
+    return patient_statistics, patient_data
+
+def extract_features(data):
+    statistics = {
+        "min" : data["activity"].min(),
+        "max" : data["activity"].max(),
+        "mean" : data["activity"].mean(),
+        "median" : data["activity"].median(),
+        "sd" : data["activity"].std(),
+        "activity_by_hour" : data[["activity", "hour"]].groupby(by="hour").mean()["activity"].to_dict()
+    }
+    return statistics
+
+def patient_plots(data):
+    plot_dir = Path(f"plots/{group_name}/{patient_num}")
+    show_plots = False
+    y = "activity"
+    if plot:
+        plots(data, x="timestamp", y=y, show=show_plots, output_path=plot_dir / "line_timestamp_vs_activity.png")
+        plots(data, x="timestamp", y=y, kind="scatter", show=show_plots, output_path=plot_dir / "scatter_timestamp_vs_activity.png")
+        plots(data, x="hour", y=y, kind="box", show=show_plots, output_path=plot_dir / "box_hour_vs_activity.png")
+        plots(data, x="hour", y=y, kind="box", show=show_plots, output_path=plot_dir / "box_hour_vs_activity_short.png")
+        plots(data, x="hour", y=y, kind="box", show=show_plots, yscale="log", output_path=plot_dir / "box_hour_vs_log_activity.png")
+
+def group_plots():
+    # Create Subdirectory for summary plots
         plots_subdir = Path('plots/summary/')
 
         # Bar plot of mean activity per hour per person in group
-        all_statistics['person_no'] = all_statistics.index
+        combined_patients_statistics['person_no'] = combined_patients_statistics.index
         plots(
-            all_statistics, x='person_no', y='mean', kind='bar', show=False, legend=False,
+            combined_patients_statistics, x='person_no', y='mean', kind='bar', show=False, legend=False,
             title= f"Bar chart of mean activity per person in the {group_name} group",
             output_path=plots_subdir / f"bar_{group_name}_mean_activity.png"
         )
 
         # Box plot of activity per person in group
         plots(
-            all_activity, x='person_no', y="activity", kind='box', legend=False,
+            all_patients_activity_data, x='person_no', y="activity", kind='box', legend=False,
             show=False,title= f"Boxplot of activity per person in the {group_name} group", 
             output_path=plots_subdir / f"box_{group_name}_activity.png"
         )
-    
-    return all_statistics, all_data
 
-
-def read_patient_file(patient_num: int, group_name: str, plot=True):
-    """Read data for each patient, calculate their individual statistics and create plots"""
-    patient_data = pd.read_csv(f"data/{group_name}/{group_name}_{patient_num}.csv")
-    patient_data["timestamp"] = pd.to_datetime(patient_data["timestamp"])
-
-    # create new column called hour
-    patient_data["hour"] = patient_data["timestamp"].dt.hour.astype(int)
-    
-    ''' if len(hour) per day less than 1440, delete day
-    '''
-
-    statistics = {
-        "min" : patient_data["activity"].min(),
-        "max" : patient_data["activity"].max(),
-        "mean" : patient_data["activity"].mean(),
-        "median" : patient_data["activity"].median(),
-        "sd" : patient_data["activity"].std(),
-        "variance" : patient_data["activity"].var(),
-        "activity_by_hour" : patient_data[["activity", "hour"]].groupby(by="hour").mean()["activity"].to_dict()
-    }
-    
-    plot_dir = Path(f"plots/{group_name}/{patient_num}")
-    show_plots = False
-
-    y = "activity"
-    if plot:
-        plots(patient_data, x="timestamp", y=y, show=show_plots, output_path=plot_dir / "line_timestamp_vs_activity.png")
-        plots(patient_data, x="timestamp", y=y, kind="scatter", show=show_plots, output_path=plot_dir / "scatter_timestamp_vs_activity.png")
-        plots(patient_data, x="hour", y=y, kind="box", show=show_plots, output_path=plot_dir / "box_hour_vs_activity.png")
-        plots(patient_data, x="hour", y=y, kind="box", show=show_plots, output_path=plot_dir / "box_hour_vs_activity_short.png")
-        plots(patient_data, x="hour", y=y, kind="box", show=show_plots, yscale="log", output_path=plot_dir / "box_hour_vs_log_activity.png")
-
-    return statistics, patient_data
-
+def combined_plots():
+    for col in ['mean', 'max', 'sd']:
+        plots(
+            combined_groups_statistics, x='group', y=col, kind='box', show=False, legend=False,
+            output_path=Path(f'plots/summary/box_ALL_{col}.png'), 
+            figsize=(10, 8),
+            title=f"Comparison of {col} activity between groups"
+        )
 
 def plots(
     df, x, y, kind="line", hue=None, figsize=(12,6), show=False, 
@@ -197,9 +234,13 @@ def plots(
         plt.show()
     plt.close()
 
-# get minutes per day
-# check for full days
-# check for how many days per full days
+
+def save_to_csv(data, path, index):
+    df = pd.DataFrame(data).copy()
+    path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(path)
+    
+    return df
 
 
 if __name__ == '__main__':
